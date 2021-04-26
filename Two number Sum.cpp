01c9f9a60d4aa67c6766288f632b59353e40f9d4
sieun-Bae <qotldms130@gmail.com>
2021-04-26 16:12:10 +0900

    Unordered set, Two pointer

--- Two number Sum.cpp
+++ Two number Sum.cpp
@@ -1,5 +1,6 @@
 // Two number Sum
 #include <vector>
+#include <unordered_set>
 #include <iostream>
 using namespace std;
 
@@ -17,6 +18,38 @@ vector<int> twoNumberSum(vector<int> array, int targetSum) {
 		}
 	}
 	return answer;
+}
+// 2. Using unordered set
+// O(N) time | O(N) space
+vector<int> twoNumberSum2(vector<int> array, int targetSum) {
+	unordered_set<int> nums;
+	for (int num:array) {
+		int potentialMatch = targetSum - num;
+		if (nums.find(potentialMatch)!=nums.end())
+			return vector<int> {potentialMatch, num};
+		else
+			nums.insert(num);
+	}
+	return {};
+}
+// 3. sort then two pointer
+// O(NlogN) time | O(1) space
+vector<int> twoNumberSum3(vector<int> array, int targetSum)
+{
+	sort(array.begin(), array.end());
+	int left=0;
+	int right=array.size()-1;
+	while (left<right) {
+		int currentSum = array[left] + array[right];
+		if (currentSum == targetSum) {
+			return {array[left], array[right]};
+		} else if (currentSum < targetSum) {
+			left++;
+		} else if (currentSum > targetSum) {
+			right--;
+		}
+	}
+	return {};
 }
 
 int main()
