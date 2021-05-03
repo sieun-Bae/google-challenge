8a5f49ad7c84d635117f6526b3d718de7e183f1a
sieun-Bae <qotldms130@gmail.com>
2021-05-03 20:20:28 +0900

    demo

---
+++ test.cpp
@@ -0,0 +1,8 @@
+beautiful matrix
+
+multiply prime num p, choose index i,
+if A1* ......... Ai == Ai+1 *.... AN then beautiful.
+minimum calculate time to make matrix A beautiful
+
+palindrome with trees
+(u,v) make string, then check is this palindrome.
